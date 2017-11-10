a6a0512018471753fd874fae34843e21df390d65
h4h13 <hemanths@live.com>
2017-11-10 23:15:36 +0530

    Code refactor

--- app/src/main/java/code/name/monkey/retromusic/util/Util.java
+++ app/src/main/java/code/name/monkey/retromusic/util/Util.java
@@ -5,32 +5,24 @@ import android.app.Activity;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Point;
-import android.graphics.PorterDuff.Mode;
 import android.graphics.drawable.Drawable;
-import android.media.audiofx.AudioEffect;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Build;
 import android.os.ResultReceiver;
-import android.provider.MediaStore;
 import android.support.annotation.AttrRes;
-import android.support.annotation.CheckResult;
 import android.support.annotation.ColorInt;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.TypedValue;
 import android.view.Display;
 import android.view.View;
 import android.view.Window;
@@ -39,24 +31,10 @@ import android.view.inputmethod.InputMethodManager;
 
 import com.kabouzeid.appthemehelper.util.TintHelper;
 
-import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Method;
-import java.util.UUID;
-
-import me.zhanghai.android.materialprogressbar.R;
 
 public class Util {
     private static final int[] TEMP_ARRAY = new int[1];
-    private static final UUID EQUALIZER_UUID;
-
-    /*http://stackoverflow.com/a/39821297*/
-    static {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-            EQUALIZER_UUID = AudioEffect.EFFECT_TYPE_EQUALIZER;
-        } else {
-            EQUALIZER_UUID = UUID.fromString("0bed4300-ddd6-11db-8f34-0002a5d5c51b");
-        }
-    }
 
     public static Uri getAlbumArtUri(long paramInt) {
         return ContentUris.withAppendedId(Uri.parse("content://media/external/audio/albumart"), paramInt);
@@ -71,102 +49,11 @@ public class Util {
         return result;
     }
 
-    public static boolean hasEqualizer() {
-        for (AudioEffect.Descriptor effect : AudioEffect.queryEffects()) {
-            if (EQUALIZER_UUID.equals(effect.type)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static Bitmap getScreenShot(View view) {
-        View screenView = view.getRootView();
-        screenView.setDrawingCacheEnabled(true);
-        Bitmap bitmap = Bitmap.createBitmap(screenView.getDrawingCache());
-        screenView.setDrawingCacheEnabled(false);
-        return bitmap;
-    }
-
-    public static void setOverflowButtonColor(final Toolbar toolbar, final int color) {
-        Drawable drawable = toolbar.getOverflowIcon();
-        if (drawable != null) {
-            drawable = DrawableCompat.wrap(drawable);
-            DrawableCompat.setTint(drawable.mutate(), color);
-            toolbar.setOverflowIcon(drawable);
-        }
-    }
-
-    public static Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    @CheckResult
-    @Nullable
-    public static Drawable createTintedDrawable(@Nullable Drawable drawable, @ColorInt int i) {
-        if (drawable == null) {
-            return null;
-        }
-        Drawable wrap = DrawableCompat.wrap(drawable.mutate());
-        DrawableCompat.setTintMode(wrap, Mode.SRC_IN);
-        DrawableCompat.setTint(wrap, i);
-        return wrap;
-    }
-
-    @CheckResult
-    @Nullable
-    public static Drawable createTintedDrawable(@Nullable Drawable drawable, @NonNull ColorStateList colorStateList) {
-        if (drawable == null) {
-            return null;
-        }
-        Drawable wrap = DrawableCompat.wrap(drawable.mutate());
-        DrawableCompat.setTintList(wrap, colorStateList);
-        return wrap;
-    }
-
     public static void openUrl(AppCompatActivity context, String str) {
         Intent intent = new Intent("android.intent.action.VIEW");
         intent.setData(Uri.parse(str));
         intent.setFlags(268435456);
         context.startActivity(intent);
-    }
-
-    public static boolean isAllowedToAutoDownload(final Context context) {
-        switch (PreferenceUtil.getInstance(context).autoDownloadImagesPolicy()) {
-            case "always":
-                return true;
-            case "only_wifi":
-                final ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
-                return netInfo != null && netInfo.getType() == ConnectivityManager.TYPE_WIFI && netInfo.isConnectedOrConnecting();
-            case "never":
-            default:
-                return false;
-        }
-    }
-
-    public static boolean getThemeAttrColor(Context context, int attr) {
-        TEMP_ARRAY[0] = attr;
-        TypedArray a = context.obtainStyledAttributes(null, TEMP_ARRAY);
-        try {
-            return a.getBoolean(0, false);
-        } finally {
-            a.recycle();
-        }
-    }
-
-    public static int getActionBarSize(@NonNull Context context) {
-        TypedValue typedValue = new TypedValue();
-        int[] textSizeAttr = new int[]{R.attr.actionBarSize};
-        int indexOfAttrTextSize = 0;
-        TypedArray a = context.obtainStyledAttributes(typedValue.data, textSizeAttr);
-        int actionBarSize = a.getDimensionPixelSize(indexOfAttrTextSize, -1);
-        a.recycle();
-        return actionBarSize;
     }
 
     public static Point getScreenSize(@NonNull Context c) {
@@ -221,23 +108,6 @@ public class Util {
         return resources.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
-    public static Drawable resolveDrawable(@NonNull Context context, @AttrRes int drawableAttr) {
-        TypedArray a = context.obtainStyledAttributes(new int[]{drawableAttr});
-        Drawable drawable = a.getDrawable(0);
-        a.recycle();
-        return drawable;
-    }
-
-    public static int resolveDimensionPixelSize(@NonNull Context context, @AttrRes int dimenAttr) {
-        TypedArray a = context.obtainStyledAttributes(new int[]{dimenAttr});
-        int dimensionPixelSize = a.getDimensionPixelSize(0, 0);
-        a.recycle();
-        return dimensionPixelSize;
-    }
-
-    public static Drawable getVectorDrawable(@NonNull Context context, @DrawableRes int id) {
-        return getVectorDrawable(context.getResources(), id, context.getTheme());
-    }
 
     public static Drawable getVectorDrawable(@NonNull Resources res, @DrawableRes int resId, @Nullable Resources.Theme theme) {
         if (Build.VERSION.SDK_INT >= 21) {
@@ -275,4 +145,6 @@ public class Util {
                 return false;
         }
     }
+
+
 }
