8c103f2d2be8bac9291588465cdb206b6b3ed19f
h4h13 <hemanths@live.com>
2017-11-10 23:10:37 +0530

    Code refactor

--- app/src/main/java/code/name/monkey/retromusic/ui/fragments/base/AbsLibraryPagerRecyclerViewFragment.java
+++ app/src/main/java/code/name/monkey/retromusic/ui/fragments/base/AbsLibraryPagerRecyclerViewFragment.java
@@ -21,6 +21,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
 import code.name.monkey.retromusic.R;
+import code.name.monkey.retromusic.helper.MusicPlayerRemote;
 import code.name.monkey.retromusic.util.ViewUtil;
 
 /**
@@ -71,9 +72,12 @@ public abstract class AbsLibraryPagerRecyclerViewFragment<A extends RecyclerView
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adapter);
 
-        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) recyclerView.getLayoutParams();
-        params.bottomMargin = getResources().getDimensionPixelOffset(R.dimen.bottom_padding);
-        recyclerView.setLayoutParams(params);
+    }
+
+    @Override
+    public void onQueueChanged() {
+        super.onQueueChanged();
+
     }
 
     protected void invalidateLayoutManager() {
