68f83b340dedc90ac15aba255f3059d4e712e891
h4h13 <hemanths@live.com>
2017-11-10 22:59:55 +0530

    Added WIKI Service

--- app/src/main/java/code/name/monkey/retromusic/lastfm/rest/LyricsClient.java
+++ app/src/main/java/code/name/monkey/retromusic/lastfm/rest/LyricsClient.java
@@ -5,34 +5,33 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
 import java.io.File;
-import java.io.IOException;
 
-import code.name.monkey.retromusic.Constants;
-import code.name.monkey.retromusic.lastfm.rest.service.KuGouApiService;
+import code.name.monkey.retromusic.lastfm.rest.service.WikiService;
 import okhttp3.Cache;
 import okhttp3.Call;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
-import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static com.retro.musicplayer.backend.RetroConstants.BASE_API_URL_WIKI;
+
 /**
- * Created by hemanths on 23/08/17.
+ * @author Hemanth S (h4h13).
  */
 
-public class KygouClient {
-    public static final String BASE_URL = Constants.BASE_API_URL_KUGOU;
+public class LyricsClient {
+    public static final String BASE_URL = BASE_API_URL_WIKI;
 
-    private KuGouApiService apiService;
+    private WikiService apiService;
 
-    public KygouClient(@NonNull Context context) {
+    public LyricsClient(@NonNull Context context) {
         this(createDefaultOkHttpClientBuilder(context).build());
     }
 
-    public KygouClient(@NonNull Call.Factory client) {
+    public LyricsClient(@NonNull Call.Factory client) {
         Retrofit restAdapter = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
                 .callFactory(client)
@@ -40,7 +39,7 @@ public class KygouClient {
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
 
-        apiService = restAdapter.create(KuGouApiService.class);
+        apiService = restAdapter.create(WikiService.class);
     }
 
     @Nullable
@@ -53,14 +52,11 @@ public class KygouClient {
     }
 
     public static Interceptor createCacheControlInterceptor() {
-        return new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request modifiedRequest = chain.request().newBuilder()
-                        .addHeader("Cache-Control", String.format("max-age=%d, max-stale=%d", 31536000, 31536000))
-                        .build();
-                return chain.proceed(modifiedRequest);
-            }
+        return chain -> {
+            Request modifiedRequest = chain.request().newBuilder()
+                    .addHeader("Cache-Control", String.format("max-age=%d, max-stale=%d", 31536000, 31536000))
+                    .build();
+            return chain.proceed(modifiedRequest);
         };
     }
 
@@ -70,7 +66,7 @@ public class KygouClient {
                 .addInterceptor(createCacheControlInterceptor());
     }
 
-    public KuGouApiService getApiService() {
+    public WikiService getApiService() {
         return apiService;
     }
 }
