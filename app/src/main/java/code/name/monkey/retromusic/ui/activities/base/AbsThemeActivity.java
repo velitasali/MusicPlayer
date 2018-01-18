a9f1aa3749e672d54dcffa59e244e5d3de9b3bb9
h4h13 <hemanths@live.com>
2018-01-18 22:42:40 +0530

    code refactor.

--- app/src/main/java/code/name/monkey/retromusic/ui/activities/base/AbsThemeActivity.java
+++ app/src/main/java/code/name/monkey/retromusic/ui/activities/base/AbsThemeActivity.java
@@ -30,12 +30,7 @@ public abstract class AbsThemeActivity extends ATHToolbarActivity {
     public void toggleFullscreenMode(boolean isFullscreen) {
         View decorView = getWindow().getDecorView();
         if (isFullscreen) {
-            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
             decorView.setSystemUiVisibility(uiOptions);
         }
     }
@@ -85,8 +80,7 @@ public abstract class AbsThemeActivity extends ATHToolbarActivity {
     }
 
     private void changeBackgroundShape() {
-        if (PreferenceManager.getDefaultSharedPreferences(this)
-                .getBoolean("corner_window", false)) {
+        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("corner_window", false)) {
             getWindow().setBackgroundDrawableResource(R.drawable.round_window);
         } else {
             getWindow().setBackgroundDrawableResource(R.drawable.square_window);
@@ -140,12 +134,13 @@ public abstract class AbsThemeActivity extends ATHToolbarActivity {
     }
 
     public void setNavigationbarColor(int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            mDecorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
-        } else if (ThemeStore.coloredNavigationBar(this)) {
+        if (ThemeStore.coloredNavigationBar(this)) {
             ATH.setNavigationbarColor(this, color);
         } else {
             ATH.setNavigationbarColor(this, Color.BLACK);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && ColorUtil.isColorLight(ThemeStore.navigationBarColor(this))) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR);
         }
     }
 
